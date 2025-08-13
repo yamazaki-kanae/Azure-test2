60c09b9f9294d10f93248b5180abdd8de553d50c
kanae_yamazaki1 <kanae_yamazaki1@waku-2.com>
2025-08-13 12:18:28 +0900

    Step2'

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,5 @@
 const { app } = require('@azure/functions');
 
-module.exports = async function (context, req) {
-    context.res.json({
-        text: "これはAPIからのメッセージです！"
-    });
-};
-
-/*app.setup({
+app.setup({
     enableHttpStream: true,
-});*/
+});
