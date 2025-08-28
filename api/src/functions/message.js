21c43e736fe32aeec56d2da588c10e702724c887
kanae_yamazaki1 <kanae_yamazaki1@waku-2.com>
2025-08-28 12:35:20 +0900

    修正

--- api/src/functions/message.js
+++ api/src/functions/message.js
@@ -4,10 +4,12 @@ app.http('message', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        context.res.json({
+        return { body: JSON.stringify({ "text": `Hello, from the API!` }) };
+
+        /*context.res.json({
             text: "これはAPIからのメッセージです！"
         });
-        /*context.log(`Http function processed request for url "${request.url}"`);
+        context.log(`Http function processed request for url "${request.url}"`);
 
         const name = request.query.get('name') || await request.text() || 'world';
 
